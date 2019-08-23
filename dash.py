94b8990af49872406a94ba8c784ab6daf888b97a
Trent Resh <tear728@gmail.com>
2019-08-23 12:46:29 -0700

    poor FPS, test standalone video

--- dash.py
+++ dash.py
@@ -5,6 +5,8 @@ cap = cv2.VideoCapture(0)
 
 while(True):
     ret, img = cap.read()
+    img = cv2.flip(img, 0)
+    img = cv2.flip(img, 1)
     cv2.imshow('img', img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
